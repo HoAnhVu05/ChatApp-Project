3b52f996f7b6d8c2908156045ea5cde1ee64815c
VanTan080405 <nguyentann080405@gmail.com>
2025-11-12 23:42:05 +0700

    ok

--- client.py
+++ client.py
@@ -33,6 +33,7 @@ def receive_messages(client_socket):
 def send_messages(client_socket):
     """Tân Sửa: Luồng để gửi lệnh từ người dùng nhé"""
     """Ok Tân"""
+    """đã sửa"""
     while True:
         try:
             user_input = input("Nhập lệnh: ")
